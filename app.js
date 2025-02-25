080d39d6fe7699f35c256798441f9959e4c434dc
Edwin Tejada <edwin.tejada1031@gmail.com>
2025-02-24 23:57:18 -0500

    Funcion Agregar Amigos: Creada

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let amigos = [];
+
+// Funcion para limpiar la caja del input
+function limpiarCaja() {
+    document.querySelector('#amigo').value = ''; 
+}
+
+// Funcion para agregar un amigo 
+function agregarAmigo() {
+    amigoAgregado = document.getElementById('amigo').value.trim();
+
+    if(amigoAgregado == ''){
+        alert('Por favor, inserte un nombre.');
+        return; // Sale de la función si el campo está vacío
+    } else {
+        // Agregar el nuevo nombre al array 
+        amigos.push(amigoAgregado); 
+    }
+    console.log(amigos);
+    //Limpia el input 
+    limpiarCaja();
+
+} 
